af2b8d62f679566dd21bc2ceb30dae3d74dede6a
Alice <awells96@rams.colostate.edu>
2026-04-21 11:16:01 -0600

    change root directories for work and scratch

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # config.py
 # Edit these lines to match your local setup.
 
-WORK_ROOT = "/glade/work/awells/air_quality/"
-SCRATCH_ROOT = "/glade/deracho/scratch/awells/air_quality/"
+WORK_ROOT = "/glade/work/awells/workflow/"
+SCRATCH_ROOT = "/glade/deracho/scratch/awells/workflow/"
 PLOTTING_ROOT = "/glade/u/home/awells/air_quality_project/plotting/"
