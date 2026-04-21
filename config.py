386e87384f9c38d64d631c10ea9fbf91ad33bbfb
Alice <awells96@rams.colostate.edu>
2026-04-21 13:25:37 -0600

    fix typo

--- config.py
+++ config.py
@@ -2,5 +2,5 @@
 # Edit these lines to match your local setup.
 
 WORK_ROOT = "/glade/work/awells/workflow/"
-SCRATCH_ROOT = "/glade/deracho/scratch/awells/workflow/"
+SCRATCH_ROOT = "/glade/derecho/scratch/awells/workflow/"
 PLOTTING_ROOT = "/glade/u/home/awells/air_quality_project/plotting/"
